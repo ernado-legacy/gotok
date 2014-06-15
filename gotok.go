84576d76e24c4d3727db3c144cbb3fe31452a04e
ernado <ernado@ya.ru>
2014-06-15 18:36:01 +0400

    init

---
+++ gotok.go
@@ -0,0 +1,80 @@
+package gotok
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+	"log"
+)
+
+type Storage interface {
+	Get(hexToken string) (*Token, error)
+	Generate(id bson.ObjectId) (*Token, error)
+	Remove(token *Token) error
+}
+
+func Generate(id bson.ObjectId) *Token {
+	var bytes = make([]byte, 100)
+	var hash = sha256.New()
+	rand.Read(bytes)
+	hash.Write(bytes)
+	hash.Write([]byte(id))
+	return &Token{id, hex.EncodeToString(hash.Sum(nil))}
+}
+
+type Token struct {
+	Id    bson.ObjectId `json:"id"     bson:"user,omitempty"`
+	Token string        `json:"token"  bson:"_id"`
+}
+
+type StorageMemory struct {
+	tokens *mgo.Collection
+	cache  map[string]bson.ObjectId
+}
+
+func (storage *StorageMemory) Get(hexToken string) (*Token, error) {
+	t := &Token{}
+	value, ok := storage.cache[hexToken]
+	if ok {
+		t.Id = value
+		t.Token = hexToken
+		return t, nil
+	}
+	err := storage.tokens.Find(bson.M{"_id": hexToken}).One(t)
+	if err == mgo.ErrNotFound {
+		return nil, nil
+	}
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	storage.cache[hexToken] = t.Id
+	return t, nil
+}
+
+func (storage *StorageMemory) Generate(id bson.ObjectId) (*Token, error) {
+	t := Generate(id)
+	err := storage.tokens.Insert(t)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	storage.cache[t.Token] = t.Id
+	return t, nil
+}
+
+func (storage *StorageMemory) Remove(token *Token) error {
+	_, ok := storage.cache[token.Token]
+	if ok {
+		delete(storage.cache, token.Token)
+	}
+	return storage.tokens.Remove(bson.M{"_id": token.Token})
+}
+
+func New(collection *mgo.Collection) *StorageMemory {
+	return &StorageMemory{collection, make(map[string]bson.ObjectId)}
+}
---
+++ gotok_test.go
@@ -0,0 +1,83 @@
+package gotok
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+	"testing"
+)
+
+func TestGotok(t *testing.T) {
+	Convey("Gotok", t, func() {
+		sesson, err := mgo.Dial("localhost")
+		So(err, ShouldBeNil)
+
+		c := sesson.DB("test").C("tokens")
+
+		Reset(func() {
+			c.DropCollection()
+		})
+
+		storage := New(c)
+
+		t, err := storage.Generate(bson.NewObjectId())
+		So(err, ShouldBeNil)
+
+		t2, err := storage.Get(t.Token)
+		So(t.Id, ShouldEqual, t2.Id)
+
+		Convey("From database", func() {
+			s := New(c)
+
+			t4, err := s.Get(t.Token)
+			So(err, ShouldBeNil)
+			So(t4.Id, ShouldEqual, t.Id)
+		})
+
+		Convey("Remove", func() {
+			err = storage.Remove(t)
+			So(err, ShouldBeNil)
+
+			t3, err := storage.Get(t.Token)
+			So(err, ShouldBeNil)
+			So(t3, ShouldBeNil)
+		})
+	})
+}
+
+func BenchmarkGet(b *testing.B) {
+	sesson, err := mgo.Dial("localhost")
+	if err != nil {
+		b.Fatal(err)
+	}
+	c := sesson.DB("test").C("tokens")
+	storage := New(c)
+	t, err := storage.Generate(bson.NewObjectId())
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	for n := 0; n < b.N; n++ {
+		storage.Get(t.Token)
+	}
+
+	c.DropCollection()
+}
+
+func BenchmarkGenerate(b *testing.B) {
+	sesson, err := mgo.Dial("localhost")
+	if err != nil {
+		b.Fatal(err)
+	}
+	c := sesson.DB("test").C("tokens")
+	storage := New(c)
+
+	for n := 0; n < b.N; n++ {
+		_, err = storage.Generate(bson.NewObjectId())
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+
+	c.DropCollection()
+}
